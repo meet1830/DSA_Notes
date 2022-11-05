8266f339ce09441188b5a86b24f8c063f01958ef
meet1830 <107630791+meet1830@users.noreply.github.com>
2022-11-05 12:10:56 +0530

    Update 287_14_Longest_Common_Prefix.java

--- 287_14_Longest_Common_Prefix.java
+++ 287_14_Longest_Common_Prefix.java
@@ -38,3 +38,49 @@ class Solution {
         return prefix;
     }
 }
+
+
+// vertical scanning
+// O(n * minLen) -> n = length of 0th string, minLen = length of shortest string in array
+// O(1)
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        for (int i = 0; i < strs[0].length(); i++) {
+            char c = strs[0].charAt(i);
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return strs[0].substring(0, i);
+                }
+            }
+        }
+        return strs[0];
+    }
+}
+
+
+// divide and conquer - recursion
+// O(n * m) best case O(n * minLen) worst case - size of all strings same
+// O(mlogn) - m space to store res, logn calls
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        return longestCommonPrefix(strs, 0, strs.length - 1);
+    }
+    private String longestCommonPrefix(String[] strs, int l, int r) {
+        if (l >= r) return strs[l];
+        int mid = l + (r - l) / 2;
+        String strsLeft = longestCommonPrefix(strs, l, mid);
+        String strsRight = longestCommonPrefix(strs, mid + 1, r);
+        return commonPrefix(strsLeft, strsRight);
+    }
+    private String commonPrefix(String s1, String s2) {
+        int minLen = Math.min(s1.length(), s2.length());
+        for (int i = 0; i < minLen; i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return s1.substring(0, i);
+            }
+        }
+        return s1.substring(0, minLen);
+    }
+}
