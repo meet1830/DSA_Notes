7c32dfa498b7d1fc4eb846ee3f9fef5c2f89b68b
meet1830 <107630791+meet1830@users.noreply.github.com>
2023-01-01 19:11:53 +0530

    Update 087_Merge_Sort_for_Linked_List.java

--- 087_Merge_Sort_for_Linked_List.java
+++ 087_Merge_Sort_for_Linked_List.java
@@ -102,31 +102,20 @@ class linkedList {
   }
   
   // iterative
-  Node merge(Node head1, Node head2) {
-    Node merged = new Node(-1);
-    Node temp = merged;
-    while(head1 != null && head2 != null) {
-      if(head1.value < head2.value) {
-        temp.next = head1;
-        head1 = head1.next;
-      }
-      else {
-        temp.next = head2;
-        head2 = head2.next;
-      }
-      temp = temp.next;
-    }
-    while(head1 != null) {
-      temp.next = head1;
-      head1 = head1.next;
-      temp = temp.next;
-    }
-    while(head2 != null) {
-      temp.next = head2;
-      head2 = head2.next;
-      temp = temp.next;
-    }
-    return merged.next;
+  private ListNode merge(ListNode head1, ListNode head2) {
+     ListNode saveHead = new ListNode(-1), temp = saveHead;
+     while (head1 != null && head2 != null) {
+         if (head1.val < head2.val) {
+             temp.next = head1;
+             head1 = head1.next;
+         } else {
+             temp.next = head2;
+             head2 = head2.next;
+         }
+         temp = temp.next;
+     }
+     temp.next = (head1 == null) ? head2 : head1;
+     return saveHead.next;
   }
   
   // recursive
